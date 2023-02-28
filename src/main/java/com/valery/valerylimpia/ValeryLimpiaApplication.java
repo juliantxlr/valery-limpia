a87f7ca3d42886ce23155cc629f456caeee22591
juliantxlr <mateoruizcan@gmail.com>
2023-02-28 18:21:13 -0500

    todo

---
+++ src/main/java/com/valery/valerylimpia/ValeryLimpiaApplication.java
@@ -0,0 +1,13 @@
+package com.valery.valerylimpia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValeryLimpiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValeryLimpiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valery/valerylimpia/ValeryLimpiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valery.valerylimpia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValeryLimpiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
